4b6c0e0e9e096f88437893dfe2964277c80a8698
陳泰恩 <3b032112@gm.student.ncut.edu.tw>
2023-10-25 08:32:03 +0800

    建立主樣板與子樣板

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+@section('title', 'Home')
+@section('content')
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>頁籤標題</title>
+
+</head>
+
+<body>
+    <h1>Home</h1>
+    <p>This my body content</p>
+</body>
+
+</html>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>@yield('title')| my blog</title>
+</head>
+
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+
+</html>
